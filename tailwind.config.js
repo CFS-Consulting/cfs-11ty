f10aff784f40cff273edf0e63e3e703ee1b1b07c
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2022-07-15 16:34:59 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,njk,md}", ".eleventy.js"],
+	theme: {
+		colors: {
+			primary: {
+				light: "rgb(var(--color-primary-light) / <alpha-value>)",
+				DEFAULT: "rgb(var(--color-primary) / <alpha-value>)",
+				dark: "rgb(var(--color-primary-dark) / <alpha-value>)",
+			},
+			accent: {
+				DEFAULT: "hsl(var(--color-accent-1) / <alpha-value>)",
+			},
+			neutral: {
+				100: "hsl(var(--color-neutral-1) / <alpha-value>)",
+				200: "hsl(var(--color-neutral-2) / <alpha-value>)",
+				300: "hsl(var(--color-neutral-3) / <alpha-value>)",
+				400: "hsl(var(--color-neutral-4) / <alpha-value>)",
+				500: "hsl(var(--color-neutral-5) / <alpha-value>)",
+			},
+			white: "hsl(var(--color-white) / <alpha-value>)",
+		},
+		fontFamily: {
+			header: "var(--font-header)",
+			body: "var(--font-body)",
+			emphasis: "var(--font-emphasis)",
+		},
+		extend: {
+			maxWidth: {
+				"70ch": "70ch",
+			},
+		},
+	},
+	plugins: [],
+};
