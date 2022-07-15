f9f40dbcc2a7451eda4925e4d8fc4a27fea0fd1a
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2022-07-15 16:43:13 -0700

    Added html minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,40 +1,5 @@
 const { DateTime } = require("luxon");
-const Image = require("@11ty/eleventy-img");
-// const htmlMinTransform = require("./src/transforms/html-min-transform.js");
-
-async function imageShortcode(src, alt, sizes = "100vw") {
-	if (alt === undefined) {
-		// You bet we throw an error on missing alt (alt="" works okay)
-		throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-	}
-
-	let metadata = await Image(src, {
-		widths: [300, 600, 900],
-		formats: ["webp", "jpeg"],
-		urlPath: "/assets/optimized/",
-		outputDir: "./_site/assets/optimized/",
-	});
-
-	let lowsrc = metadata.jpeg[0];
-	let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
-
-	return `${Object.values(metadata)
-		.map((imageFormat) => {
-			return `  <source type="${
-				imageFormat[0].sourceType
-			}" srcset="${imageFormat
-				.map((entry) => entry.srcset)
-				.join(", ")}" sizes="${sizes}">`;
-		})
-		.join("\n")}
-		<img
-		  src="${lowsrc.url}"
-		  width="${highsrc.width}"
-		  height="${highsrc.height}"
-		  alt="${alt}"
-		  loading="lazy"
-		  decoding="async">`;
-}
+const htmlMinTransform = require("./src/transforms/html-min-transform.js");
 
 // Eleventy module exports
 module.exports = function (eleventyConfig) {
